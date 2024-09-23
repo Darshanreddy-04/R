96d8732e36ba6f12b247fdb49a2675bc9e89ea9b
Darshanreddy-04 <darshanreddycp04@gmail.com>
2024-09-23 12:31:20 +0530

    Add files via upload

---
+++ control str.R
@@ -0,0 +1,31 @@
+a=as.integer(readline())
+b=as.integer(readline())
+if(a>b){
+  print(a-b)
+  } else{
+    print(a-b)
+  }
+ 
+
+a=as.integer(readline())
+b=as.integer(readline())
+if(a>b){
+  print("a is less than b")
+} else if (a<b)
+{
+  print("a is greater than b")
+}
+
+num3=as.integer(readline())
+if(num3>0)
+  {
+  if(num%%2==0)
+  print("even+ve")
+} else if ("odd+ve")
+  {
+  else if (num3<0)
+  
+  print("value is even")
+}
+?switch
+
